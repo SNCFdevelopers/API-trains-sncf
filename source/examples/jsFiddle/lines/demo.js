4ed22696e4f435ccf96444ea30fd051e6a3ade2e
Julien Maulny <doubjulien@hotmail.fr>
2016-05-26 15:39:17 +0200

    Update demo.js

--- source/examples/jsFiddle/lines/demo.js
+++ source/examples/jsFiddle/lines/demo.js
@@ -1,5 +1,5 @@
 // Url to retrieve lines available on the coverage
-var linesUrl = 'http://vip-jormsim-ws.mutu.prod.canaltp.fr/v1/coverage/sandbox-sncf/lines';
+var linesUrl = 'http://navitia2sim-ws.mutu.production.canaltp.fr/v1/coverage/sandbox-sncf/lines';
 
 // Call SNCF API
 $.ajax({
